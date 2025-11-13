020e83f9809c36eb57b479157b6c27268eabab4c
Atig-Hamza <hamzaatig@icloud.com>
2025-11-13 16:35:19 +0100

    chore(Routes): setup routes handller

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Tirelire from './pages/Tirelire.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div className="App">hello world!</div>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tirelire" element={<Tirelire />} />
+    </Routes>
+  )
 }
 
 export default App
