0af4b477cb861f2142730b0ce8396b50b8aaa2eb
Atig-Hamza <hamzaatig@icloud.com>
2025-11-19 10:25:46 +0100

    feat(page): create 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+    return (
+        <div className="h-screen w-screen bg-white flex items-center justify-center">
+            <h1 className="text-6xl font-bold text-gray-800">404 - Page Not Found</h1>
+        </div>
+    );
+}   
+
+export default NotFound;
