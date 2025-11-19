31222fb22128be7f420fb9f868d77c2d26c06884
Atig-Hamza <hamzaatig@icloud.com>
2025-11-19 10:21:53 +0100

    feat(home): create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/footer";
+import HeroLandingPage from "../components/HeroLandingPage";
+
+export default function Home() {
+  return (
+    <>
+      <HeroLandingPage />
+      <Footer />
+    </>
+  )
+}
