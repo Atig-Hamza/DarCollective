851f6c1dc5a16c7570a4b324fb9a95187607779b
Atig-Hamza <hamzaatig@icloud.com>
2025-11-19 10:21:10 +0100

    feat(app): add routes of login and signup and also 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Tirelire from './pages/Tirelire.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import NotFound from './pages/404.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tirelire" element={<Tirelire />} />
+      <Route path="login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="*" element={<NotFound/>} />
     </Routes>
   )
 }
