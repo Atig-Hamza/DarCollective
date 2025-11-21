c3073bedd0194912518842fbb8ad40eb95380830
Atig-Hamza <hamzaatig@icloud.com>
2025-11-21 14:52:51 +0100

    feat(routes): add AuthMiddleware to dashboard and tirelire routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Tirelire from './pages/Tirelire.jsx'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import NotFound from './pages/404.jsx'
+import AuthMiddleware from './middleware/authMiddleware.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/tirelire" element={<Tirelire />} />
+      <Route path="/dashboard" element={<AuthMiddleware><Dashboard/></AuthMiddleware>} />
+      <Route path="/tirelire" element={<AuthMiddleware><Tirelire/></AuthMiddleware>} />
       <Route path="login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>} />
       <Route path="*" element={<NotFound/>} />
