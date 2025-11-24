45a31976c6ba6e2b8d212014e4baf04a03a88f52
Atig-Hamza <hamzaatig@icloud.com>
2025-11-24 12:00:03 +0100

    feat(routes): add profile route with AuthMiddleware for access control

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup.jsx'
 import NotFound from './pages/404.jsx'
 import AuthMiddleware from './middleware/authMiddleware.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import ProfilePage from './pages/Profile.jsx'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<AuthMiddleware><Dashboard/></AuthMiddleware>} />
       <Route path="/tirelire" element={<AuthMiddleware><Tirelire/></AuthMiddleware>} />
+      <Route path="/profile" element={<AuthMiddleware><ProfilePage/></AuthMiddleware>} />
       <Route path="login" element={<Login/>}/>
       <Route path="/signup" element={<Signup/>} />
       <Route path="*" element={<NotFound/>} />
