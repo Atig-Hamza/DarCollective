968ef6f49cd9a3fe61e68b4484dfc4afbf49ab53
Atig-Hamza <hamzaatig@icloud.com>
2025-12-08 09:19:22 +0100

    feat(Tirelire): import and render TirelireOverview component

--- src/pages/Tirelire.jsx
+++ src/pages/Tirelire.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SideBar from '../components/SideBare';
+import TirelireOverview from '../components/TirelireOverview';
 
 
 
@@ -7,7 +8,7 @@ const Tirelire = () => {
     return (
         <div className="flex min-h-screen bg-gray-50 font-sans text-gray-900">
             <SideBar />
-            
+            <TirelireOverview />
         </div>
     );
 };
